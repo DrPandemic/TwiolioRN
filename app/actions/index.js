8a1d7015f851aee8595cc974c114bae54d67c20c
DrPandemic <bipbip500@gmail.com>
2017-06-07 09:43:52 +0200

    Links message reducer

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,8 +1,10 @@
 import * as fetchedAccountNumbersActions from './fetchedAccountNumbers';
 import * as accountActions from './account';
+import * as messageActions from './messages';
 
 export const ActionCreators = Object.assign(
   {},
   fetchedAccountNumbersActions,
   accountActions,
+  messageActions,
 );
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,18 +3,22 @@
 import { combineReducers } from 'redux-loop';
 import * as fetchedAccountNumbersReducers from './fetchedAccountNumbers';
 import * as accountReducers from './account';
+import * as messageReducers from './messages';
 
 export const reducer = combineReducers({
   fetchedAccountNumbers: fetchedAccountNumbersReducers.reducer,
   account: accountReducers.reducer,
+  messages: messageReducers.reducer,
 });
 
-export type StateT = {
+export type StateT = {|
   fetchedAccountNumbers: fetchedAccountNumbersReducers.T,
   account: accountReducers.T,
-};
+  messages: messageReducers.T,
+|};
 
 export const initialState: StateT = {
   fetchedAccountNumbers: fetchedAccountNumbersReducers.initialState,
   account: accountReducers.initialState,
+  messages: messageReducers.initialState,
 };
