65c09b197133cae27a3ab6ae316b6dcfa130cfa3
DrPandemic <bipbip500@gmail.com>
2017-06-08 16:37:05 +0200

    Removes icon lib

--- android/app/src/main/java/com/twiliorn/MainApplication.java
+++ android/app/src/main/java/com/twiliorn/MainApplication.java
@@ -3,7 +3,6 @@ package com.twiliorn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new ReactNativeConfigPackage()
       );
     }
