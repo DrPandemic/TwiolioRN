d6180a83986faf003dfb3e1677fb3ae7afb00463
DrPandemic <bipbip500@gmail.com>
2017-06-12 15:43:45 +0200

    Adds function test to the matcher function

--- app/lib/__tests__/match.test.js
+++ app/lib/__tests__/match.test.js
@@ -22,3 +22,26 @@ test('match', () => {
   // in matcher && multiple elements && not last
   expect(match({ a: 2 }, 'foo', [['a'], 3], [{ a: 2 }, 42], [1, 3])).toEqual(42);
 });
+
+test('function matchers', () => {
+  // default
+  expect(match([1, 2, 3], 42, [e => e.length === 4, 'foo'])).toEqual(42);
+  // in matcher && single element
+  expect(match([1, 2, 3], 'foo', [e => e.length === 3, 42])).toEqual(42);
+  // in matcher && multiple elements
+  expect(match([1, 2, 3], 'foo',
+               [e => e[0] === 0, 'foo'],
+               [e => e.length === 3, 42])
+  ).toEqual(42);
+  // in matcher && multiple elements && not last
+  expect(match([1, 2, 3], 'foo',
+               [e => e.length === 3, 42],
+               [e => e[0] === 0, 'foo'])
+  ).toEqual(42);
+  // in matcher && multiple elements && non function matcher
+  expect(match([1, 2, 3], 'foo',
+               [e => e.length === 4, 'foo'],
+               [e => e[0] === 0, 'foo'],
+               [[1, 2, 3], 42])
+  ).toEqual(42);
+});
--- app/lib/match.js
+++ app/lib/match.js
@@ -8,9 +8,14 @@ export default function match(
   [matcher, matcherValue]: [any, any],
   ...rest: Array<[ any, any ]>
 ): any {
-  if (equal(value, matcher)) {
+  if (typeof matcher === 'function' && matcher(value)) {
+    // The matcher is a function and returned true
+    return matcherValue;
+  } else if (equal(value, matcher)) {
+    // The matcher works
     return matcherValue;
   } else if (rest.length === 0) {
+    // No more matcher, use default
     return defaultValue;
   }
   return match(value, defaultValue, rest.pop(), ...rest);
