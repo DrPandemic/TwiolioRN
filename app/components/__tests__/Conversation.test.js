28f9ae12b2ad4e2d0f6731bc48cc37946d20f2aa
DrPandemic <bipbip500@gmail.com>
2017-06-13 15:31:28 +0200

    Moves Conversation integration tests

--- app/components/__tests__/Conversation.test.js
+++ app/components/__tests__/Conversation.test.js
@@ -1,11 +1,13 @@
 // @flow
 
+import React from 'react';
 import renderer from 'react-test-renderer';
 
 import { PConversation } from '../Conversation';
 import messageFixture from '../../test_helpers/fixtures/received_message.json';
 import { Message } from '../../types';
 import { addMessages, getMessagesById } from '../../types/ConversationStore';
+import { initialState as mInitial } from '../../reducers/messages';
 
 test('renders a message', () => {
   const message = new Message(messageFixture.simple);
@@ -25,4 +27,39 @@ test('renders rows', () => {
   const messages = getMessagesById(store, message.conversationId);
 
   expect(renderer.create(PConversation.renderRows(messages))).toMatchSnapshot();
+});
+
+test('renders an empty conversation', () => {
+  const list = renderer.create(
+      <PConversation
+        messages={{ ...mInitial }}
+        location={{ state: 'foo' }}
+      >
+      </PConversation>
+  ).toJSON();
+
+  expect(list).toMatchSnapshot();
+});
+
+test('renders an non-empty conversation', () => {
+  const message = new Message(messageFixture.simple);
+  const store = addMessages({}, [
+    message,
+    new Message(messageFixture.simpleInverse),
+    new Message(messageFixture.simpleOtherFrom),
+    new Message(messageFixture.secondOutbound),
+  ]);
+
+  const list = renderer.create(
+      <PConversation
+        messages={{
+          ...mInitial,
+          messages: store,
+        }}
+        location={{ state: message.conversationId }}
+      >
+      </PConversation>
+  ).toJSON();
+
+  expect(list).toMatchSnapshot();
 });
